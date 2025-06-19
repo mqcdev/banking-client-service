2a00d37d0f4baf19fb2c435fe13e69eb50f6d3d5
mqcdev <michael.chavezjq@gmail.com>
2025-06-19 01:42:13 -0500

    feat(consumer): add the loan consumer focused on kafka

---
+++ src/main/java/com/nttdata/banking/client/consumer/LoanConsumer.java
@@ -0,0 +1,32 @@
+package com.nttdata.banking.client.consumer;
+
+import com.nttdata.banking.client.infrastructure.LoanRepository;
+import com.nttdata.banking.client.model.Loan;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LoanConsumer {
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @KafkaListener(topics = "${spring.kafka.topic.loan.name}")
+    public void listener(@Payload Loan loan) {
+        log.debug("Message received : {} ", loan);
+        applyListLoans(loan).blockFirst();
+    }
+
+    private Flux<Loan> applyListLoans(Loan request) {
+        log.debug("applyListLoans executed : {} ", request);
+        return loanRepository.findLoanByDocumentNumber(
+                request.getClient().getDocumentNumber());
+    }
+}
